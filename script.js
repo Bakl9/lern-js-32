b0cd95cc2fa0fb86e92714806b744b9855bc620d
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-18 10:13:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,54 @@ user.sayHi = function() {
 };
 
 alert.sayHi();
+
+// user = {
+//     sayHi: function() {
+//         alert('Привет');
+//     }
+// };
+
+// user = {
+//     sayHi () {
+//         alert('Privet')
+//     }
+    
+// }
+
+
+// let user = {
+//     name: "John",
+//     age: 30,
+
+//     sayHi(){
+//         alert(This.name)
+//     }
+
+// }
+
+// user.sayHi()
+
+
+// let user = {
+//     name: "Джон",
+//     age: 30,
+
+//     sayHi() {
+//         alert (user.name);
+
+//     }
+// };
+
+
+// let admin = user;
+// user = null
+
+// admin.sayHi();
+
+
+let user = {name:"Джон"};
+let admin = {name:"Админ"};
+
+function sayHi() {
+    alert (this.name);
+}
